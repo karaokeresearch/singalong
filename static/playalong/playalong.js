f3cd4297e0e16c4945da3ebc34be2bda9aeb78d1
karaokeresearch <krc@brassrocket.com>
2014-10-28 11:40:28 -0700

    Basic version of the finger strummer works!  Needs a little work to be less glitchy, but it is cool! Added a few virtual instruments.

--- static/playalong/playalong.js
+++ static/playalong/playalong.js
@@ -3,7 +3,25 @@
 	var playalong  = {};
 	
 	playalong.init = function (sock) {
-		
+	var socket=sock;
+
+	setInterval(function () { //the queue
+		while ((playQueue.length>0) &&(playQueue[0][0] - ntp.serverTime() <500)){//although it's tested every 250 ms, we can schedule up to 500ms away.
+			(function (){
+				var whatsnext=playQueue[0][1];
+				setTimeout(function (){
+					changeChord(whatsnext);
+				}, (playQueue[0][0] - ntp.serverTime()));
+			}());
+			playQueue.shift();
+		}
+	},250);
+	
+				
+	socket.on('bnext', function (data) { //what's the next chord and when
+			playQueue.push([parseInt(data.nextChange)-lagOverride,data.nextChord])
+	});
+
 		
 	};
 
