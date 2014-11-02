179bdd5bdac34108e866a79c2d0ac89284072bd0
karaokeresearch <krc@brassrocket.com>
2014-11-01 21:05:35 -0700

    lag determine rolled into singalong. Will need to modularize eventually.

--- scheduler/scheduler.js
+++ scheduler/scheduler.js
@@ -15,8 +15,15 @@ io.set('log level', 1); // reduce logging
 
 app.use('/ua/', function(req, res, next){
   var ua = req.headers['user-agent'];
-  res.send(parser.setUA(ua).getResult());
-  res.end;
+  var phone= parser.setUA(ua).getResult();
+  var tosend='';
+	findClosestModel(phone, function(callback){
+		tosend +=("closest phone match is " +callback);
+		findClosestBrowser(phone.browser.version, phone, callback, function(callback){
+		res.send(tosend + "<br>lag be: " +callback);
+		res.end;
+		});
+	});
   //we could also just be sent the ua from the client property "navigator.userAgent"
 });
 
@@ -30,7 +37,7 @@ console.log ("Starting on Port 80");
 var db = new Datastore({ filename: './useragents.nedb', autoload: true });
 	
 
-var phone = { 
+var phonetest = { 
   "ua": "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.124 Safari/537.36",
   "browser": {
     "name": "Chrome",
@@ -46,61 +53,13 @@ var phone = {
     "version": "4.0"                                                                                              
   },                                                                                                             
   "device": {
-  	"model": "Whizzhard Palace",
+  	"model": "FOOBLAT90210",
   	"vendor": "Samsung",
   	"type": "mobile"
   	},                                                                                                  
   "cpu": {}                                                                                                                                                                                                                    
                                                                                                                 
 };
-
-//From most to least important:
-
-
-
-
-//model, vendor, os.name, browser.name&&browser.version, os.version
-//if model, (look through all results and look for the ones with the exact os.name/major.minor version){levenshtein browser + browser version.} else{ levenshtein browser + browser version on full data set}.
-//if vendor, (look through all results and look for the ones with the exact os.name/major.minor version){levenshtein browser + browser version.} else{ levenshtein browser + browser version on full data set}.
-//if os.name (look through all results and look for the ones with the exact os.name/major.minor  version){levenshtein browser + browser version.} else{ levenshtein browser + browser version on full data set}.
-//otherwise, you're screwed.  You can't derive any useful information. Assume a default of 200ms
-
-//if browser.name&&browser. 
-
-//db.insert(phone, function (err, newDoc) {   // Callback is optional
-  // newDoc is the newly inserted document, including its _id
-  // newDoc has no key called notToBeSaved since its value was undefined
-//});	
-//
-
-////db.find({"device.vendor": phone.device.vendor}, function (err, docs) {
-//db.find({"device.model": phone.device.model}, function (err, docs) {    
-//	if (docs.length >0){                                                  
-//		console.log("model:");                                              
-//		docs.forEach(logArrayElements);                                     
-//	}else{                                                                
-//		db.find({"device.vendor": phone.device.vendor}, function (err, docs) {
-//			if (docs.length >0){                                              
-//				console.log("devices from vendor " + phone.device.vendor + ":");
-//				docs.forEach(logArrayElements);                                 
-//			}else{                                                            
-//				db.find({"os.name": phone.os.name}, function (err, docs) {      
-//                                                                        
-//					if (docs.length >0){                                          
-//						console.log("os name:");                                    
-//						docs.forEach(logArrayElements);                             
-//					}else{                                                        
-//						$ms=200;                                                    
-//					}                                                             
-//				});                                                             
-//			}                                                                 
-//		});                                                                 
-//	}                                                                     
-//});                                                                     
-//                                                                        
-//});
-
-var i=0;  
 
 
 var findClosestModel = function(whichPhone, callback){
@@ -111,12 +70,12 @@ var findClosestModel = function(whichPhone, callback){
 			db.find({"device.vendor": whichPhone.device.vendor}, function (err, docs) { 
 			var lowestOne=Infinity;
 			var bestModel;
-			docs.forEach(function(element, index, array){
-				if (new Levenshtein(element.device.model,whichPhone.device.model) <lowestOne){
-					lowestOne=new Levenshtein(element.device.model,whichPhone.device.model);
-					bestModel=element.device.model;
+			for (j=0; j<docs.length; j++){
+				if (new Levenshtein(docs[j].device.model,whichPhone.device.model) <lowestOne){
+					lowestOne=new Levenshtein(docs[j].device.model,whichPhone.device.model);
+					bestModel=docs[j].device.model;
 					}
-			});
+		}
 			callback(bestModel);      
 			});
 	  }
@@ -124,11 +83,6 @@ var findClosestModel = function(whichPhone, callback){
 };
 
 
-//var findClosestBrowser = function(whichPhone, whichModel, callback){
-//			var version=whichPhone.browser.version;	
-//			console.log("looking for: " + version);
-//			findClosestBrowser(version, whichPhone, whichModel, callback);
-//};
 
 function sortByKey(array, key) {
     return array.sort(function(a, b) {
@@ -149,7 +103,7 @@ var findClosestBrowser = function(version, whichPhone, whichModel, callback, ref
 		//console.log(docs);
       
 		if (docs.length){//we found one or more candidates
-			console.log("refinement is " + refinement);
+			//console.log("refinement is " + refinement);
 			
 			for (var i = 0; i < docs.length; i++) { 
 				docs[i].diff = Math.abs((refinement||0)- ((docs[i].browser.version.split(".")[String(refinement).split(".").length-1])||0));
@@ -169,22 +123,14 @@ var findClosestBrowser = function(version, whichPhone, whichModel, callback, ref
 				}
 			
 			lag/=i+1; //average
-			
-			//console.log("selectedDocs: ");
-			//console.log(selectedDocs);
-			//console.log("lag (" + docs.length+ "): "+lag);
-
-			
-		//console.log("found it: " +versionRegEx +"\n\n");
-			//console.log(docs[0].browser.name + " " + docs[0].browser.version)
 			callback(lag);
 		}else{
 			var previousVersion=version;
 			version= version.replace(/(\.|^)\w+$/,"");
 			if (version===previousVersion){
-				console.log("not found")
+				//console.log("not found")
 			}else{
-				console.log("replacing");
+				//console.log("replacing");
 				refinement = parseInt(previousVersion.replace(/.*\./,""));
 				findClosestBrowser(version, whichPhone, whichModel, callback, refinement);
 			}
@@ -194,108 +140,11 @@ var findClosestBrowser = function(version, whichPhone, whichModel, callback, ref
 }
 
 
-
-
-
-
-findClosestModel(phone, function(callback){
-	console.log ("closest phone match is " +callback);
-	findClosestBrowser(phone.browser.version, phone, callback, function(callback){
-		console.log (callback);
-	});
-	
-});
-		
-
-//db.find({"device.model": phone.device.model}, function (err, docs) {      
-//	if (docs.length >0){                                                    
-//		console.log("match on model:");
-//		
-//
-//                                           
-//		docs.forEach(logArrayElement);                                       
-//	}else{                                                                  
-//		db.find({"device.vendor": phone.device.vendor}, function (err, docs) {
-//			if (docs.length >0){                                                
-//				console.log("match on vendor " + phone.device.vendor + ":");  
-//				docs.forEach(logArrayElement);                                   
-//			}else{                                                              
-//				db.find({"os.name": phone.os.name}, function (err, docs) {        
-//                                                                          
-//					if (docs.length >0){                                            
-//						console.log("Match on OS name " + phone.os.name + ":");                                      
-//						docs.forEach(logArrayElement);                               
-//					}else{                                                          
-//						$ms=200;                                                      
-//					}                                                               
-//				});                                                               
-//			}                                                                   
-//		});                                                                   
-//	}                                                                       
-//});                                                                       
-//
-
-//var compareVersions= function(v1, v2){
-//	if (v1.split(".").length>v2.split(".").length){
-//		var r1=v1;
-//		var r2=v2;
-//	}else{
-//		var r2=v1;
-//		var r1=v2;
-//	}	
-//	
-//	var r1a = r1.split(".");
-//	var r2a = r2.split(".");
-//	var score=0;
-//	
-//	r1a.forEach(function(e,i,a){
-//		if (e!==r2a[i]){
-//			v=Math.abs(e-r2a[i]);
-//			if (!isNaN(v)){
-//				normalizedV=(v*Math.pow(10, -(v.toString().length)+1));
-//				}
-//			else{
-//				normalizedV=1;
-//			}	
-//			score +=(Math.pow(10,-i) * normalizedV);
-//			}
+//findClosestModel(phone, function(callback){
+//	console.log ("closest phone match is " +callback);
+//	findClosestBrowser(phone.browser.version, phone, callback, function(callback){
+//		console.log (callback);
 //	});
-//	return score;
-//}
-//
-//var s2="1.2.54";
-//var s1 ="11.20.9.4";
-//console.log(compareVersions(s2,s1));
-
-
-
-//
-//  OK, new idea:  cycle through and add the version number array to each one.  Figure out how to sort by that in increasing 
-//  http://stackoverflow.com/questions/2784230/javascript-how-do-you-sort-an-array-on-multiple-columns            way
-//  with the candidate JSON thrown in there.  Sort, identify where yours is, and then assign a number to each element in terms of relation to 
-//  the compared unit. i.e. 2.0.332.0  you could then assign each digit a percentage of the min and max.  the pervious element might become 
-//  2/16 0/0 332/600 0/0 i.e 1.050.  Take this and add it all together and sort and the top candidate is your ms choice
-// maybe this is bad.
-
-
-//ok, here's an easy idea: sort each one by version number, find the closest match and then average the three resulting MSes. If no matches, exclude the results
-
-
-
-
-                                                                             	
-//(look through all results and look for the ones with the exact os.name/major.minor version){levenshtein browser + browser version.} else{ levenshtein browser + browser version on full data set}.
-//actually, you don't need to levenstein.  You need a function that pushes through the subversions looking for exact matches until it finds nothing and then simply finding the closest integer. Should be extensible to an infinte number of periods.
-function logArrayElement(element, index, array) {
-//put a line here that assigns the phone's major.minor to a variable to test against below
-//db.find({ $and: [{ "os.name": phone.os.name }, { "os.version": phone.os.version }] }, function (err, docs) {
-
-//  });
-//  var l = new Levenshtein(a,b)
-	i=i+1;
-  console.log('a[' + index + '] = ' + element.device.vendor + " " + element.device.model + ", Levenshtein: "+ new Levenshtein(element.device.model,phone.device.model) );   
-  console.log (element.os.name + " " + element.os.version + ", " + element.engine.name + " " + element.engine.version + ", " + element.browser.name + " " + element.browser.version);
-	console.log(i + ' ----------' );
- }                                                 //var parser = new UAParser();
-// var ua = request.headers['user-agent'];     // user-agent header from an HTTP request
-// console.log(parser.setUA(ua).getResult());
+//	
+//});
+//               
--- singalong.js
+++ singalong.js
@@ -20,10 +20,24 @@ var express = require('express'),
     app = express(),
     server = http.createServer(app).listen(80),
     ntp = require('socket-ntp-krcmod'),
+    Datastore = require('nedb'),
+    UAParser  = require('ua-parser-js'),
+	  sortzzy = require('sortzzy');
+
     io = require('socket.io').listen(server, {
         log: false
     });
+
 io.sockets.on('connection', ntp.sync); //ntp server
+
+
+
+
+var db = new Datastore({ filename: './useragents.nedb', autoload: true });
+var parser = new UAParser();
+
+
+
 var fs = require("fs");
 var os = require('os');
 var installedSongsDirectory = __dirname + '/songs/';
@@ -105,6 +119,8 @@ var loadNewSong=function(newsong){ //loads the appropriate file (or the index) i
 
     }
 }
+
+
 
 var returnIndexHTML=function(callback){ //Spit out the index based on the list of files in the /songs subfolder
     console.log("---------------------\nReading files from: \n" + songsDirectory + "\n");
@@ -390,6 +406,185 @@ var dirExistsSync=function(d){
 
 
 
+
+		
+
+
+
+
+var versionToInt=function(whatver){ //a terrible function that makes a version number into a pseudo integer
+	if (whatver){
+		var modver = whatver.split(".");
+		var finalver = '';
+		for (var i=0; i<3; i++){
+			if(typeof modver[i] === 'undefined') {
+				finalver += '000';
+			}
+			else{
+				if(isNaN(parseInt(modver[i]))){var tempver="";}else{var tempver=String(parseInt(modver[i]));}
+					if (tempver.length>3){tempver= tempver.substr(0,3);}
+					for (j=0; j<(3-tempver.length); j++){
+						finalver += '0'; //pad with zeroes
+					}
+					finalver += tempver;
+				}
+			}
+			return parseInt(finalver);
+			}else{return 0};
+		}
+
+
+var sortExtractLagData= function(data, model, callback){
+
+	//console.log(data);
+
+if (data.length>0){
+	var lowestOSVersionNumber=Infinity;
+	var highestOSVersionNumber=0;
+
+	var lowestEngineVersionNumber=Infinity;
+	var highestEngineVersionNumber=0;
+
+	var lowestBrowserVersionNumber=Infinity;
+	var highestBrowserVersionNumber=0;
+
+
+	//decimalize all version data for results, discover boundaries for different results
+	for (var i=0; i<data.length; i++){
+		data[i].OSVersionDecimal=versionToInt(data[i].os.version);
+		if (data[i].OSVersionDecimal<lowestOSVersionNumber){lowestOSVersionNumber=data[i].OSVersionDecimal;}
+		if (data[i].OSVersionDecimal>highestOSVersionNumber){highestOSVersionNumber=data[i].OSVersionDecimal;}
+
+		data[i].engineVersionDecimal=versionToInt(data[i].engine.version);
+		if (data[i].engineVersionDecimal<lowestEngineVersionNumber){lowestEngineVersionNumber=data[i].engineVersionDecimal;}
+		if (data[i].engineVersionDecimal>highestEngineVersionNumber){highestEngineVersionNumber=data[i].engineVersionDecimal;}
+
+		data[i].browserVersionDecimal=versionToInt(data[i].browser.version);
+		if (data[i].browserVersionDecimal<lowestBrowserVersionNumber){lowestBrowserVersionNumber=data[i].browserVersionDecimal;}
+		if (data[i].browserVersionDecimal>highestBrowserVersionNumber){highestBrowserVersionNumber=data[i].browserVersionDecimal;}
+
+		//some baloney because sortzzy can't handle nested JSON
+		data[i].deviceModel=data[i].device.model;
+		data[i].engineName=data[i].engine.name;
+		data[i].browserName=data[i].browser.name;
+
+	//if (data[i].device.model===undefined){console.log("undefined"); data[i].device.model="";}
+	//if (data[i].device.vendor===undefined){data[i].device.vendor=""}
+
+	}
+	
+	//decimalize the test data, expand the boundaries if test data requires it.
+	model.OSVersionDecimal=versionToInt(model.os.version);
+	if (model.OSVersionDecimal<lowestOSVersionNumber){lowestOSVersionNumber=model.OSVersionDecimal;}
+	if (model.OSVersionDecimal>highestOSVersionNumber){highestOSVersionNumber=model.OSVersionDecimal;}
+
+	model.engineVersionDecimal=versionToInt(model.engine.version);
+	if (model.engineVersionDecimal<lowestEngineVersionNumber){lowestEngineVersionNumber=model.engineVersionDecimal;}
+	if (model.engineVersionDecimal>highestEngineVersionNumber){highestEngineVersionNumber=model.engineVersionDecimal;}
+
+	model.browserVersionDecimal=versionToInt(model.browser.version);
+	if (model.browserVersionDecimal<lowestBrowserVersionNumber){lowestBrowserVersionNumber=model.browserVersionDecimal;}
+	if (model.browserVersionDecimal>highestBrowserVersionNumber){highestBrowserVersionNumber=model.browserVersionDecimal;}
+
+	if (model.device.model===undefined){model.device.model=""}
+	if (model.device.vendor===undefined){model.device.vendor=""}
+
+	model.deviceModel=model.device.model;
+	model.engineName=model.engine.name;
+	model.browserName=model.browser.name;
+
+
+//the numerical search won't work if there's no range
+  if (lowestOSVersionNumber === highestOSVersionNumber){highestOSVersionNumber++;}
+	if (lowestEngineVersionNumber === highestEngineVersionNumber){highestEngineVersionNumber++;}
+  if (lowestBrowserVersionNumber === highestBrowserVersionNumber){highestBrowserVersionNumber++;}
+
+
+	// THE IMPORTANT PART.  Changing the weights below determines how the algorithm decides which lag value is probably the best.
+	var fields = [];
+	if(model.deviceModel!==undefined){fields.push({name:'deviceModel', type:'string', weight:8, options:{ignoreCase:true}});}	
+	if(model.os.version!==undefined){fields.push({name:'OSVersionDecimal', type:'numeric', weight:6, fixedRange:[lowestOSVersionNumber, highestOSVersionNumber]});}
+	if(model.engine.name!==undefined){fields.push({name:'engineName', type:'string', weight:3, options:{ignoreCase:true}});}
+	if(model.engine.version!==undefined){fields.push({name:'engineVersionDecimal', type:'numeric', weight:3, fixedRange:[lowestEngineVersionNumber, highestEngineVersionNumber]});}
+	if(model.browser.name!==undefined){fields.push({name:'browserName', type:'string', weight:2, options:{ignoreCase:true}});}
+	if(model.browser.version!==undefined){fields.push({name:'browserVersionDecimal', type:'numeric', weight:2, fixedRange:[lowestBrowserVersionNumber, highestBrowserVersionNumber]});}
+
+	var results= sortzzy.sort(data, model, fields);
+
+
+	var avgLag=0;
+	var bestScore=results[0].score;
+	var i=0;
+	while (i<results.length && bestScore===results[i].score) {
+		avgLag+=parseInt(results[i].data.lag);
+		i++;
+	}
+	avgLag=parseInt(avgLag/i);
+	callback(avgLag, results[0]);
+}else{callback(200,'');} //it should never come to this, since the function should always be passsed data.  But just in case...
+}
+
+
+
+
+
+
+
+var determineLag=function(model, callback){	
+	
+	if (model.device.model!==undefined && model.device.vendor!==undefined){//is it a mobile device
+		var queryLiteral = [{ "device.model": model.device.model},{ "device.vendor": model.device.vendor}];
+		db.find({ $and: queryLiteral }, function (err, docs) {
+			if (docs.length){//we found one or more matches for exact device model
+				//console.log("exact match");
+				sortExtractLagData(docs, model, function(lag,bestresult){
+					callback(lag);
+				});
+			}else{//look for any results from the vendor
+	      //console.log('looking for vendor ' + model.device.vendor);
+				var queryLiteral = { "device.vendor": model.device.vendor};
+				db.find(queryLiteral, function (err, docs) {
+					if (docs.length){//we found one or more matches for vendor
+						//console.log("vendor match");
+						sortExtractLagData(docs, model, function(lag,bestresult){
+							callback(lag);
+							//consol	e.log(bestresult);
+						});
+					}else{
+						callback(200);
+						//console.log("no vendor match. 200");
+					}
+				});
+			}
+	
+		});
+	
+	}else{ //it's not a mobile device
+	
+		var queryLiteral = {"os.name": model.os.name};
+		db.find(queryLiteral, function (err, docs) {
+			if (docs.length){//we found one or more matches for exact device model
+			
+				sortExtractLagData(docs, model, function(lag,bestresult){
+					//console.log("it's a PC!");
+					callback(lag);
+					//console.log(bestresult);
+				});
+			}else{
+				 callback(200)
+				//console.log("none for your OS found. 200");
+			}
+		});
+	
+	}
+};
+
+
+
+
+
+
+
 //************** BEGIN PROGRAM ********************
 if (dirExistsSync(songsDirectory)===false) { //some sync business up front. Create and populate a local songs directory if none exists.
     console.log(songsDirectory + " doesn't exist. Creating.");
@@ -452,6 +647,17 @@ app.get('/timings', function (req, res) { //JSON timings object
     res.end(JSON.stringify(timings));
 
 });
+
+
+app.use('/ua/', function(req, res, next){
+  var ua = req.headers['user-agent'];
+	var model= parser.setUA(ua).getResult();
+	  determineLag(model, function(lag){
+ 			res.end(JSON.stringify({lag:lag}));
+		});
+});
+
+
 
 app.use(express.static(__dirname + '/static')); //Where the static files are loaded from
 app.use('/audio', express.static(audioDirectory)); //Where the static files are loaded from
